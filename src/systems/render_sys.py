2b13f1e4257e37338c6a9b3518c9bca68d98c34c
Victor Lemos de Freitas <victorlemosfr@gmail.com>
2025-12-10 18:44:36 -0300

    feat/UI

--- src/systems/render_sys.py
+++ src/systems/render_sys.py
@@ -1,4 +1,6 @@
 import pygame
+import src.systems.ui as UI
+import src.objects.entity.obj_player as obj_player
 
 class RenderSystem:
     def __init__(self, screen, camera):
@@ -6,6 +8,9 @@ class RenderSystem:
         self.camera = camera
         # LayeredUpdates organize sprites in layers
         self.render_group = pygame.sprite.LayeredUpdates()
+        self.ui = UI.UI()
+        self.player = obj_player.Player("Edísio", 300, 300, "src/sprites/psg.png")
+
 
     def add_sprite(self, sprite, layer=0):
         # Adding sprite and the layer sprite to system of renderizing group
@@ -22,6 +27,9 @@ class RenderSystem:
             screen_pos = self.camera.apply(sprite.rect)
             # Desenha a imagem do sprite naquela posição
             self.screen.blit(sprite.image, screen_pos)
+
+            # Desenha a UI
+            self.ui.display(self.player)
 
             # (Debug) Drawning the reactangle of sprite
             # pygame.draw.rect(self.screen, (255,0,0), screen_pos, 1)

6fd6d572f32cebd6007d274ac7ce0231ed32e82a
Gabriel Cassio Gomes Cileiro <gcgc@windows.cin.ufpe.br>
2025-12-15 08:18:37 -0300

    feat(fix/melee-attack):fixed

--- src/objects/components/obj_movement_command.py
+++ src/objects/components/obj_movement_command.py
@@ -3,10 +3,10 @@ from src.objects.entity.obj_entity import Entity
 
 class MovementCommand:
 
-    def __init__(self, character: Entity, dx, dy, velocity = 5):
+    def __init__(self, character: Entity, dx, dy, player_speed = 5):
         # Keeping the instance ogf character
         self.character = character
-        self.velocity = velocity
+        self.speed = player_speed
         self.position_x = self.character.rect.x
         self.position_y = self.character.rect.y
 
@@ -18,11 +18,11 @@ class MovementCommand:
             self.direction = self.direction.normalize()
         
     def execute(self):
-        self.position_x += (self.direction.x * self.velocity)
-        self.position_y += (self.direction.y * self.velocity)
+        self.position_x += (self.direction.x * self.speed)
+        self.position_y += (self.direction.y * self.speed)
         self.character.move(self.position_x, self.position_y)
     
     def undo(self):
-        rev_position_x = (self.position_x - (self.direction.x * self.velocity))
-        rev_position_y = (self.position_y - (self.direction.x * self.velocity)) 
+        rev_position_x = (self.position_x - (self.direction.x * self.speed))
+        rev_position_y = (self.position_y - (self.direction.x * self.speed)) 
         self.character.move(rev_position_x, rev_position_y)
